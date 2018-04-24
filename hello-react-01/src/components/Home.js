7b60af945a6ca542133cdbd0a583f9da627deac1
ahkjxy <ahkjxy@qq.com>
2018-04-24 17:18:25 +0800

    variables

--- hello-react-01/src/components/Home.js
+++ hello-react-01/src/components/Home.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
+    let num = 1,
+      content;
+    if (num === 1) {
+      content = 'Hello str1'
+    } else {
+      content = 'Hello str2'
+    }
     return (
       <div className="col-xl-1 col-xl-offset-11">
         <h1>Home</h1>
+        { content }
       </div>
     );
   }
