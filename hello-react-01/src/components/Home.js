a440c822ef815e81d355efc75e1bdb4ab66632ae
ahkjxy <ahkjxy@qq.com>
2018-04-25 11:45:32 +0800

    conditional state

--- hello-react-01/src/components/Home.js
+++ hello-react-01/src/components/Home.js
@@ -5,7 +5,8 @@ export default class Home extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      age: this.props.age
+      age: this.props.age,
+      isOpen: false
     }
   }
   myAgeAdd () {
@@ -13,8 +14,14 @@ export default class Home extends Component {
       age: this.state.age + 3
     })
   }
+  setOpen () {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
   render() {
-    let children = this.props.children;
+    const children = this.props.children,
+      isOpen = this.state.isOpen;
     return (
       <div className="col-xl-1 col-xl-offset-11">
         <h1>Home</h1>
@@ -32,6 +39,16 @@ export default class Home extends Component {
           <ul>
             {this.props.user.hobbies.map((item, idx) => <li key={idx}>{item}</li>)}
           </ul>
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" onClick={()=>{this.setOpen()}}>
+              Dropdown button
+            </button>
+            <div className={isOpen ? "dropdown-menu show" : "dropdown-menu"}>
+              <a className="dropdown-item" onClick={()=>this.setState({isOpen:false})}>Action</a>
+              <a className="dropdown-item">Another action</a>
+              <a className="dropdown-item">Something else here</a>
+            </div>
+          </div>
         </div>
         {children}
       </div>
